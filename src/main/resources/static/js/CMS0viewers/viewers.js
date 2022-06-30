7643a75d211f1e817de1d60fa7b485b3cf36b8e1
Chanwu77 <a1687336@gmail.com>
2022-06-30 12:07:15 +0800

    0630版本

---
+++ src/main/resources/static/js/CMS0viewers/viewers.js
@@ -0,0 +1,11 @@
+// 瀏覽人數
+$(function(){
+    $.ajax({
+        url:"/security/countToken",
+        type:"POST",
+        data:"void",
+        success:function(data){
+            countpeople.innerText = data.countRs; 
+        }
+    })
+})
